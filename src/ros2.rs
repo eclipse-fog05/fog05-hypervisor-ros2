1024cd7bae431e01a052a061fc9b6ceb75499533
gabrik <gabriele.baldoni@gmail.com>
2021-01-08 16:15:34 +0100

    use of find_local_servers to find agent, os and networking services
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- src/ros2.rs
+++ src/ros2.rs
@@ -765,13 +765,13 @@ impl ROS2Hypervisor {
         async_std::channel::Sender<()>,
         async_std::task::JoinHandle<FResult<()>>,
     ) {
-        let local_os = OSClient::find_servers(self.z.clone()).await.unwrap();
+        let local_os = OSClient::find_local_servers(self.z.clone()).await.unwrap();
         if local_os.is_empty() {
             log::error!("Unable to find a local OS interface");
             panic!("No OS Server");
         }
 
-        let local_agent = AgentPluginInterfaceClient::find_servers(self.z.clone())
+        let local_agent = AgentPluginInterfaceClient::find_local_servers(self.z.clone())
             .await
             .unwrap();
         if local_agent.is_empty() {
@@ -779,7 +779,7 @@ impl ROS2Hypervisor {
             panic!("No Agent Server");
         }
 
-        let local_net = NetworkingPluginClient::find_servers(self.z.clone())
+        let local_net = NetworkingPluginClient::find_local_servers(self.z.clone())
             .await
             .unwrap();
         if local_net.is_empty() {
